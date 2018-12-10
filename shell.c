5951efc6a3627179f6d9cc58497003975b76a4f1
Imad <ibelkebir@stuy.edu>
2018-12-10 15:22:25 -0500

    testing

--- shell.c
+++ shell.c
@@ -250,7 +250,13 @@ int main(){
             		run = 1;
       	      }
       	      if(!(strcmp(line[j], "|"))){
-            		redirect_pipe(line);
+                int f = fork();
+                if(f){
+                  redirect_pipe(line);
+                }else{
+                  int status;
+                  wait(&status);
+                }
             		run = 1;
       	      }
       	    }
