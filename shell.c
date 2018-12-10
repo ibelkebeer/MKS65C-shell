cf820b2f8caf1d30e34344faf1a3f26c721dc45a
Imad <ibelkebir@stuy.edu>
2018-12-10 15:08:10 -0500

    testing

--- shell.c
+++ shell.c
@@ -183,8 +183,14 @@ void redirect_pipe(char** line){
   }else{
     close(fds[1]);
     dup2(fds[0], 0);
-    if(execvp(command2[0], command2) == -1){
-       printf("Error: %s\n", strerror(errno));
+    f = fork();
+    if(f){
+      if(execvp(command2[0], command2) == -1){
+	       printf("Error: %s\n", strerror(errno));
+      }
+    }else{
+      int status;
+      wait(&status);
     }
   }
 }
