002b8f584486be412d7a97664791f053d897ff14
Imad <ibelkebir@stuy.edu>
2018-12-11 13:08:19 -0500

    testing

--- shell.c
+++ shell.c
@@ -263,6 +263,7 @@ int main(){
                 }else{
                   int status;
                   wait(&status);
+                  printf("HI\n");
                 }
             		run = 1;
                 dup2(backup_in, STDIN_FILENO);
