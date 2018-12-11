78238c9dee585034e034005055ffd0c0818b6169
Imad <ibelkebir@stuy.edu>
2018-12-11 11:39:25 -0500

    testing

--- shell.c
+++ shell.c
@@ -266,7 +266,7 @@ int main(){
             		run = 1;
                 dup2(backup_in, STDIN_FILENO);
                 dup2(backup_out, STDOUT_FILENO);
-                fflush(stdout);
+                fflush(stdin);
       	      }
       	    }
       	  }
