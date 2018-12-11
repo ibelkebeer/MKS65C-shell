51af8436e898d857708842b461e096c296126e92
Imad <ibelkebir@stuy.edu>
2018-12-11 12:51:40 -0500

    testing

--- shell.c
+++ shell.c
@@ -267,8 +267,7 @@ int main(){
             		run = 1;
                 dup2(backup_in, STDIN_FILENO);
                 dup2(backup_out, STDOUT_FILENO);
-                printf("\n");
-                getchar();
+                printf("HI");
       	      }
       	    }
       	  }
